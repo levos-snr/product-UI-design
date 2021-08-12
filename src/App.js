cba0826fdb3ff86bb2a1bc808c92c00b684bf7b0
Lewis Odero <75755882+tripple-boy@users.noreply.github.com>
2021-08-13 01:47:06 +0300

    product UI design

---
+++ src/App.js
@@ -0,0 +1,36 @@
+/** @format */
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="card">
+          <div className="imgBx">
+            <img src="../download.png" />
+          </div>
+          <div className="contentBx">
+            <h2>Nike Shoes</h2>
+            <div className="size">
+              <h3>Size :</h3>
+              <span>5</span>
+              <span>7</span>
+              <span>11</span>
+              <span>13</span>
+            </div>
+            <div class="color">
+              <h3>Color :</h3>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <a href="#">Buy Now </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
